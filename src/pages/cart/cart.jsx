3054d615fc76a6c0d9eb1b75e0ca6ce16a92429b
Balasaiteja Ganesu <139602876+BalasaitejaG@users.noreply.github.com>
2024-06-07 23:03:50 +0530

    adding elements to the cart is done....still things to be done

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,8 +1,49 @@
-// import React from "react";
+import React, { useContext } from "react";
 import "./cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { food_list } from "../../assets/assets";
 
 const cart = () => {
-  return <div>cart</div>;
+  const { cartItems, food_List, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <>
+                <div
+                  key={item._id}
+                  className="cart-items-title cart-items-item"
+                >
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default cart;
