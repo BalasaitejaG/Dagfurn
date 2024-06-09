000180507f564b979cb7e25563dce7a1e2db2d95
Balasaiteja <balasaiteja17@gmail.com>
2024-06-09 20:29:59 +0530

    doing some chedkaniya in cart.jsx (fixong prices those are shown)

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from "react";
 import "./cart.css";
 import { StoreContext } from "../../context/StoreContext";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -55,12 +55,14 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() + 2}</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
           <button onClick={() => navigate("/order")}>
