79135c0e7692b8408bfdf7235613addc6273d790
Balasaiteja <balasaiteja17@gmail.com>
2024-06-09 20:50:03 +0530

    linked #Explore menu to the view menu button at header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.css'
 const header = () => {
+  const[menu,setMenu]=useState('menu')
   return (
     <div className='header'>
         <div className='headerContents'>
             <h2>Order your favorite foods form here</h2>
             <p>Get your favorite foods at your doorstep</p>
-            <button>View Menu</button>
+            <button><a href="#ExploreMenu" onClick={() => setMenu("menu")}>View Menu</a></button>
         </div>
     </div>
   )
