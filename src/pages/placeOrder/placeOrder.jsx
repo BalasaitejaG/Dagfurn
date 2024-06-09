f161ca3da5942be1214fef17e87c0b263dc2df6a
Balasaiteja <balasaiteja17@gmail.com>
2024-06-09 20:19:11 +0530

    changing in place order section

--- src/pages/placeOrder/placeOrder.jsx
+++ src/pages/placeOrder/placeOrder.jsx
@@ -1,9 +1,52 @@
-// import React from 'react'
-import './placeOrder.css'
+import React, { useContext } from "react";
+import "./placeOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 const placeOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
-    <div>placeOrder</div>
-  )
-}
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email Address" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="phone" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Sub Total</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>Proceed to Payment</button>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default placeOrder
+export default placeOrder;
